0f39b50b47fbf749bc4ff05ba37d9016fbd0b8f8
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-07-31 18:24:58 +0900

    hellowworld

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HellowWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("김찬은 밥반찬");
+            
+        }
+    }
+}
